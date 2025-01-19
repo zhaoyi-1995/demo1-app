7d4a20246866a2aaecbb1f4b20b8899d17969915
zhaoyi-1995 <492613075@qq.com>
2025-01-19 14:34:21 +0800

    我的

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import axios from 'axios'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState('你好')
+  const handleData =  async () => {
+    const res = await axios.get('https://my-project-name.zy492613075.workers.dev/')
+    console.log(res)
+    setText(res.data.content)
+  }
+
+  // useEffect(() => {
+  //   handleData()
+  // }, [])
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handleData}>获取数据</button>
+      {text}
     </>
   )
 }
